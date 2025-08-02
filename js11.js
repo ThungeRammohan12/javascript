d88063c471b6849272fd66a3b61c6cc3fb8bfce7
THUNGERAMMOHAN <76059035+THUNGERAMMOHAN@users.noreply.github.com>
2025-08-02 14:58:39 +0530

    file added

---
+++ js11.js
@@ -0,0 +1,68 @@
+// var num1=prompt("enter the number:");
+// var num2=prompt("enter the second number");
+// var num3=prompt("enter the third number:");
+// if(num1>num2 && num1>num3){
+//     console.log(`${num1} is greatest number`)
+// }
+// else if(num2>num1 && num2>num3){
+//     console.log(`${num2} is the greatest number`)
+// }
+// else{
+//     console.log(`${num3}is the greatest number`)
+// }
+
+
+
+
+// var num1=prompt("enter a number:")
+// if(num1%3==0 && num1%5==0){
+//     console.log("the number is divisibe by both 3 abd 5 ")
+// }
+// else if(num1%3==0){
+//     console.log("the number is divisible by 3")
+// }
+// else if(num1%5==0){
+//     console.log("the number is divisble by 5")
+// }
+// else{
+//     console.log("the number is not divisibl 3and 5")
+// }
+
+
+
+// let a=Number(prompt("enter the first number"));
+// let b=Number(prompt("enter the second number"));
+// let opr = prompt("enter the operations");
+// switch(opr){
+//     case "+":
+//         console.log(a+b);
+//         break;
+//     case "-":
+//         console.log(a-b);
+//         break;
+//     case "*":
+//         console.log(a*b);
+//         break;
+//     case "/":
+//         console.log(a/b);
+//         break;
+//     default:
+//         console.log("enter the valid operation");
+// }
+
+
+
+
+// // Sum of first N natural numbers
+// n=prompt("enter the number")
+// sum1=0
+// for(i=1;i<=n;i++){
+//     sum1=sum1+i;
+// }""
+// console.log(sum1)
+
+n=prompt("enter the number");
+for(i=1;i<=10;i++){
+    console.log(~${n}*${i}=${n*i}~)
+}
+
