41eef8804acecbed322f122830422ee95c1da647
THUNGERAMMOHAN <76059035+THUNGERAMMOHAN@users.noreply.github.com>
2025-08-03 16:37:53 +0530

    functions are commited

---
+++ functions.js
@@ -0,0 +1,173 @@
+// Write a named function that prints "Good Morning" to the console
+// function greeting(){
+//     console.log("good morning");
+// }
+
+// greeting()
+
+// function greeting(name){
+//         console.log("welcome  "+ name);
+// }
+// greeting("ram")
+
+//  Write a named function that takes two numbers and prints their sum.
+
+
+// a=Number(prompt("enter the number"));
+// b=Number(prompt("enter the second number:"));
+// function addition(a,b){
+//     console.log(a+b);
+// }
+// addition(a,b)
+
+
+
+//  Write a named function that checks if a number is even and prints true or false.
+
+
+// function even(n){
+//     if(n%2==0){
+//         console.log("True");
+//     }
+//     else{
+//         console.log("False");
+//     }
+// }
+// n=Number(prompt("enter  the number:"));
+// even(n)
+
+
+
+// Q5. Write a named function that loops from 1 to 5 and prints each number.
+
+
+// function sequence(n){
+//     for(i=1;i<=5;i++){
+//         console.log(i);
+//     }
+// }
+// n=Number(prompt("enter the number:"));
+// sequence(n)
+
+
+// Q6. Write an arrow function to print "I love JavaScript".
+
+// let sayLove=()=>{
+//     console.log(" i love javascript")
+// }
+// sayLove()
+
+
+//  Write an arrow function that multiplies two numbers and prints the result.
+
+// let multipli=(a,b)=>{
+//     console.log(a*b);
+// }
+// multipli(2,3)
+
+// Q8. Write an arrow function that checks if a number is negative.
+
+
+// let sign=(n)=>{
+//     if(n>0){
+//         console.log("positive number");
+//     }
+//     else{
+//         console.log("negative number");
+//     }
+// }
+// n=prompt("enter the number");
+// sign(n)
+
+// 9. Write an arrow function that takes a number and prints whether it is odd/even.
+// let even=(n)=>{
+//     if(n%2==0){
+//         console.log("even number");
+//     }
+//     else{
+//         console.log("odd number");
+//     }
+// }
+// even(11)
+
+
+// Q10. function greet() {
+//   console.log("Hello, friend!");
+// }
+
+// let greet=()=>{
+//     console.log("hello friend");
+// }
+// greet()
+
+
+// Q11. function square(num) {
+//   console.log(num * num);
+// }
+
+
+// let square=(num)=>{
+//         console.log(num*num);
+// }
+// square(5)
+
+
+// function isPositive(n) {
+//   console.log(n > 0);
+// }
+// Arrow Function:
+
+// javascript
+// Copy
+// Edit
+// const isPositive = (n) => {
+//   console.log(n > 0);
+// };
+
+// isPositive(3);  // Output: true
+
+
+
+// function displayAge(name, age) {
+//   console.log(`${name} is ${age} years old.`);
+// }
+// Arrow Function:
+
+// javascript
+// Copy
+// Edit
+// const displayAge = (name, age) => {
+//   console.log(`${name} is ${age} years old.`);
+// };
+
+// displayAge("Ram", 22);  
+
+
+// Q15.  const sayHi = () => console.log("Hi there!");
+
+// function sayHi(){
+//     console.log("HI there!")
+// }
+// sayHi()
+
+// Q16.  const cube = x => console.log(x ** 3);
+
+// function cube(x){
+//     console.log(x**3);
+// }
+// x=Number(prompt("enter the number"));
+// cube(x)
+
+
+
+// Q17.  const printName = name => console.log("Name is:", name);
+
+// function printName(name){
+//     console.log("Name is ",name);
+// }
+// name=prompt("enter your name");
+// printName(name)
+
+
+// Q18. const isEven = num => console.log(num % 2 === 0);
+
